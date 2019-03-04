9d777a180b876f7aa21d6a06cd8ff561254f674c
Angela Rosas <gelita@users.noreply.github.com>
2019-03-04 14:38:53 -0800

    updated background color changed programmatically instead of in xml layout file

--- app/src/main/java/com/fanikiosoftware/moodtracker/MainActivity.java
+++ app/src/main/java/com/fanikiosoftware/moodtracker/MainActivity.java
@@ -1,7 +1,8 @@
 package com.fanikiosoftware.moodtracker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,10 +12,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setActivityBackgroundColor(R.color.banana_yellow);
         setContentView(R.layout.activity_main);
         ivMood = findViewById(R.id.ivMood);
         ivMood.setImageResource(R.drawable.great_mood);
-//        ivGreat = findViewById(R.id.ivGreat);
-//        ivGreat.setImageResource(R.drawable.great_mood);
+    }
+
+    public void setActivityBackgroundColor(int color) {
+        View view = this.getWindow().getDecorView();
+        view.setBackgroundResource(color);
     }
 }
