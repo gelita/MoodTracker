bd6074db042b9686370be45dfb0244b78c1977e2
Angela Rosas <gelita@users.noreply.github.com>
2019-03-04 13:33:03 -0800

    adding mood icons and updating Main Activity layout w/buttons and mood bg image

--- app/src/main/java/com/fanikiosoftware/moodtracker/MainActivity.java
+++ app/src/main/java/com/fanikiosoftware/moodtracker/MainActivity.java
@@ -2,12 +2,19 @@ package com.fanikiosoftware.moodtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ivMood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ivMood = findViewById(R.id.ivMood);
+        ivMood.setImageResource(R.drawable.great_mood);
+//        ivGreat = findViewById(R.id.ivGreat);
+//        ivGreat.setImageResource(R.drawable.great_mood);
     }
 }
