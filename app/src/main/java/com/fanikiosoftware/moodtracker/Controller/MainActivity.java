35df83a9709072640fd031c0dc7d2d983187ab56
Angela Rosas <gelita@users.noreply.github.com>
2019-03-19 16:21:19 -0700

    updated onTouch() in main

--- app/src/main/java/com/fanikiosoftware/moodtracker/Controller/MainActivity.java
+++ app/src/main/java/com/fanikiosoftware/moodtracker/Controller/MainActivity.java
@@ -65,6 +65,7 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener,
                 btnConfirm.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        saveMood();
                         saveComment();
                         dialogBuilder.dismiss();
                     }
@@ -89,50 +90,52 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener,
             }
         });
     }
-        private void checkMood() {
-            setContentView(R.layout.activity_main);
-            ivMood = findViewById(R.id.ivMood);
-            int mood = mPreferences.getInt(PREF_KEY_MOOD, 0);
-            //if a mood has been stored, display,
-            //else, default mood
-            if (mood < 0) {
-                switch(mood){
-                    case BANANA_YELLOW:
-                        ivMood.setImageResource(R.drawable.great_mood);
-                        view.setBackgroundResource(R.color.banana_yellow);
-                        break;
-                    case LIGHT_SAGE:
-                        ivMood.setImageResource(R.drawable.good_mood);
-                        view.setBackgroundResource(R.color.light_sage);
-                        break;
-                    case CORNFLOWER_BLUE:
-                        ivMood.setImageResource(R.drawable.decent_mood);
-                        view.setBackgroundResource(R.color.cornflower_blue);
-                        break;
-                    case WARM_GREY:
-                        ivMood.setImageResource(R.drawable.bad_mood);
-                        view.setBackgroundResource(R.color.warm_grey);
-                        break;
-                    case  FADED_RED:
-                        ivMood.setImageResource(R.drawable.sad_mood);
-                        view.setBackgroundResource(R.color.faded_red);
-                        break;
-                }
-            }else {
-                //set default mood and color if no mood was previously saved
-                ivMood.setImageResource(R.drawable.sad_mood);
-                view.setBackgroundResource(R.color.faded_red);
+
+    private void checkMood() {
+        setContentView(R.layout.activity_main);
+        ivMood = findViewById(R.id.ivMood);
+        int mood = mPreferences.getInt(PREF_KEY_MOOD, 0);
+        //if a mood has been stored, display,
+        //else, default mood
+        if (mood < 0) {
+            switch (mood) {
+                case BANANA_YELLOW:
+                    ivMood.setImageResource(R.drawable.great_mood);
+                    view.setBackgroundResource(R.color.banana_yellow);
+                    break;
+                case LIGHT_SAGE:
+                    ivMood.setImageResource(R.drawable.good_mood);
+                    view.setBackgroundResource(R.color.light_sage);
+                    break;
+                case CORNFLOWER_BLUE:
+                    ivMood.setImageResource(R.drawable.decent_mood);
+                    view.setBackgroundResource(R.color.cornflower_blue);
+                    break;
+                case WARM_GREY:
+                    ivMood.setImageResource(R.drawable.bad_mood);
+                    view.setBackgroundResource(R.color.warm_grey);
+                    break;
+                case FADED_RED:
+                    ivMood.setImageResource(R.drawable.sad_mood);
+                    view.setBackgroundResource(R.color.faded_red);
+                    break;
             }
+        } else {
+            //set default mood and color if no mood was previously saved
+            ivMood.setImageResource(R.drawable.great_mood);
+            view.setBackgroundResource(R.color.banana_yellow);
+        }
     }
 
     //save memo from popup input dialog when user clicks on CONFIRM
     public void saveComment() {
-        String comment = editText.getText().toString().trim();
-        mPreferences.edit().putString(PREF_KEY_MEMO, comment).apply();
-        System.out.println("PREF_KEY_MEMO: " + comment);
-        int mood = ((ColorDrawable) view.getBackground()).getColor();
-        mPreferences.edit().putInt(PREF_KEY_MOOD, mood).apply();
-        System.out.println("PREF_KEY_MOOD: " + mood);
+        mPreferences.edit().putString(PREF_KEY_MEMO, editText.getText().toString().trim()).apply();
+    }
+
+    //save the mood that is currently selected
+    public void saveMood(){
+        mPreferences.edit().putInt(PREF_KEY_MOOD,
+                ((ColorDrawable) view.getBackground()).getColor()).apply();
     }
 
     @Override
@@ -140,9 +143,9 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener,
         mGestureDetector.onTouchEvent(event);
         return super.onTouchEvent(event);
     }
-
     @Override
     public boolean onTouch(View v, MotionEvent event) {
+        v.performClick();
         return false;
     }
 
@@ -152,15 +155,16 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener,
         //did event get handled here? Assume the negative
         boolean result = false;
         //gauge movement along y axis for determining if user swiped vertically
+        //subtract the starting position on the Y axis from the ending position on Y axis
         float diffY = moveEvent.getY() - downEvent.getY();
         //ignore all non-vertical swipe gestures
         //handle vertical swipes
         if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
             int color = ((ColorDrawable) view.getBackground()).getColor();
             if (diffY > 0) {
-                onSwipeBottom(color);
+                onSwipeDownwards(color);
             } else {
-                onSwipeTop(color);
+                onSwipeUpwards(color);
             }
             //gesture was handled within this method so result changed to TRUE
             result = true;
@@ -168,31 +172,8 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener,
         return result;
     }
 
-    private void onSwipeBottom(int color) {
-        switch (color) {
-            case BANANA_YELLOW:
-                view.setBackgroundResource(R.color.light_sage);
-                ivMood.setImageResource(R.drawable.good_mood);
-                break;
-            case LIGHT_SAGE:
-                view.setBackgroundResource(R.color.cornflower_blue);
-                ivMood.setImageResource(R.drawable.decent_mood);
-                break;
-            case CORNFLOWER_BLUE:
-                view.setBackgroundResource(R.color.warm_grey);
-                ivMood.setImageResource(R.drawable.bad_mood);
-                break;
-            case WARM_GREY:
-                view.setBackgroundResource(R.color.faded_red);
-                ivMood.setImageResource(R.drawable.sad_mood);
-                break;
-            case FADED_RED:
-                //do nothing - user must swipe upwards to change back to previous mood
-                break;
-        }
-    }
-
-    public void onSwipeTop(int color) {
+    //mood changes from bad to good as user swipes upward
+    private void onSwipeUpwards(int color) {
         switch (color) {
             case FADED_RED:
                 view.setBackgroundResource(R.color.warm_grey);
@@ -211,11 +192,35 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener,
                 ivMood.setImageResource(R.drawable.great_mood);
                 break;
             case BANANA_YELLOW:
-                //do nothing- user must swipe up to go back through moods
+                //do nothing-user must swipe down to go back to lesser mood
                 break;
         }
     }
 
+    //mood changes from good to bad as user swipes downward
+    public void onSwipeDownwards(int color) {
+        switch (color) {
+            case BANANA_YELLOW:
+                view.setBackgroundResource(R.color.light_sage);
+                ivMood.setImageResource(R.drawable.good_mood);
+                break;
+            case LIGHT_SAGE:
+                view.setBackgroundResource(R.color.cornflower_blue);
+                ivMood.setImageResource(R.drawable.decent_mood);
+                break;
+            case CORNFLOWER_BLUE:
+                view.setBackgroundResource(R.color.warm_grey);
+                ivMood.setImageResource(R.drawable.bad_mood);
+                break;
+            case WARM_GREY:
+                view.setBackgroundResource(R.color.faded_red);
+                ivMood.setImageResource(R.drawable.sad_mood);
+                break;
+            case FADED_RED:
+                //do nothing -user must swipe up to go back to better mood
+                break;
+        }
+    }
     @Override
     public boolean onDown(MotionEvent e) {
         return false;
@@ -260,7 +265,7 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener,
     @Override
     protected void onPause() {
         super.onPause();
-       // mPreferences.edit().putInt(PREF_KEY_MOOD, ((ColorDrawable) view.getBackground()).getColor()).apply();
+        saveMood();
         out.println("MainActivity::onPause()");
     }
 
